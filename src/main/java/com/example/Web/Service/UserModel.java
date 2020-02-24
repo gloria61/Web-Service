3a7e859def41335cde9062ad93149da5f86023f4
gloria61 <ozogizabella@gmail.com>
2020-02-24 18:02:04 +0000

    initial

---
+++ src/main/java/com/example/Web/Service/UserModel.java
@@ -0,0 +1,55 @@
+package com.example.Web.Service;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+public class UserModel implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idStudent;
+	
+	private String nnname;
+
+	public Long getIdStudent() {
+		return idStudent;
+	}
+
+	public void setIdStudent(Long idStudent) {
+		this.idStudent = idStudent;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public UserModel(Long idStudent, String name) {
+		super();
+		this.idStudent = idStudent;
+		this.name = name;
+	}
+	
+	public UserModel() {
+		
+	}
+}
---
+++ src/main/java/com/example/Web/Service/WebServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.Web.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class WebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceApplication.class, args);
+	}
+
+}
