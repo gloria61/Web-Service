a9127f3feefb00e6d89e6e430dea8c3e5a039bb4
gloria61 <ozogizabella@gmail.com>
2020-02-25 17:41:10 +0000

    go back to previous v

---
+++ src/main/java/com/example/Web/Service/UserModel.java
@@ -0,0 +1,46 @@
+package com.example.Web.Service;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+public class UserModel implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idStudent;
+	
+
+	public Long getIdStudent() {
+		return idStudent;
+	}
+
+	public void setIdStudent(Long idStudent) {
+		this.idStudent = idStudent;
+	}
+
+
+	public UserModel(Long idStudent, String name) {
+		super();
+		this.idStudent = idStudent;
+	}
+	
+	public UserModel() {
+		
+	}
+}
