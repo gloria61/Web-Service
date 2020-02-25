0a7dfffaca2fac01ae47d64b17c87a1916c567f9
gloria61 <ozogizabella@gmail.com>
2020-02-25 17:46:15 +0000

    Revert "simplify more"
    This reverts commit f13e9c6888695d52829c1dceeb58ee1a8db78b7a.

--- src/main/java/com/example/Web/Service/UserController.java
+++ src/main/java/com/example/Web/Service/UserController.java
@@ -1,27 +1,25 @@
 package com.example.Web.Service;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-
-//import java.util.List;
-
-//import javax.validation.Valid;
-
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//import com.example.Web.Service.UserRepository;
-//import com.example.Web.Service.UserModel;
+import com.example.Web.Service.UserRepository;
+import com.example.Web.Service.UserModel;
 
 @RestController
 public class UserController {
 	
-	//@Autowired
-	//UserRepository repository;
+	@Autowired
+	UserRepository repository;
 	
-	/*@GetMapping("/users")
+	@GetMapping("/users")
 	public List<UserModel> getAllUsers(){
 		return repository.findAll();
 	}
@@ -29,11 +27,6 @@ public class UserController {
 	@PostMapping("/users")
 	public UserModel createUser(@Valid @RequestBody UserModel user) {
 		return repository.save(user);
-	}*/
-	
-	@GetMapping("/hello")
-	public void printHelo() {
-		System.out.println("hi bitch");
 	}
 
 }
