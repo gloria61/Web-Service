2d2890261565eacf54b6b9d6c60de88d59d97e89
gloria61 <ozogizabella@gmail.com>
2020-03-10 14:31:13 +0000

    Add all columns to users table

--- src/main/java/com/example/Web/Service/UserModel.java
+++ src/main/java/com/example/Web/Service/UserModel.java
@@ -26,6 +26,16 @@ public class UserModel implements Serializable {
     private Long idStudent;
 	
 	private String name;	
+	private String surname;
+	private int age;
+	private String email;
+	private int gender;
+	private int sexuality;
+	private int preference;
+	private String department;
+	private int phoneNumber;
+	private double points;
+	private String password;
 
 	public Long getIdStudent() {
 		return idStudent;
@@ -42,12 +52,103 @@ public class UserModel implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public String getSurname() {
+		return surname;
+	}
 
-	public UserModel(Long idStudent, String name) {
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public int getSexuality() {
+		return sexuality;
+	}
+
+	public void setSexuality(int sexuality) {
+		this.sexuality = sexuality;
+	}
+
+	public int getPreference() {
+		return preference;
+	}
+
+	public void setPreference(int preference) {
+		this.preference = preference;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public double getPoints() {
+		return points;
+	}
+
+	public void setPoints(double points) {
+		this.points = points;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	public UserModel(Long idStudent, String name, String surname, int age, String email, int gender, int sexuality,
+			int preference, String department, int phoneNumber, double points, String password) {
 		super();
 		this.idStudent = idStudent;
 		this.name = name;
-	
+		this.surname = surname;
+		this.age = age;
+		this.email = email;
+		this.gender = gender;
+		this.sexuality = sexuality;
+		this.preference = preference;
+		this.department = department;
+		this.phoneNumber = phoneNumber;
+		this.points = points;
+		this.password = password;
 	}
 	
 	public UserModel() {
